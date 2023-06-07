5e9190fedbc9d833a09c28421cb779343daa6f69
BunnyeNyash <otachibosibori@gmail.com>
2023-06-07 12:52:23 -0700

    rm

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        ascii_val = ord(char)
-        if ord('a') <= ascii_val <= ord('z'):
-            ascii_val -= ord('a') - ord('A')
-        print(chr(ascii_val), end="")
-    print()
