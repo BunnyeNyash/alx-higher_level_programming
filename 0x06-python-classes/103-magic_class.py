fc950f11c9cdf74b525f235216496481a6b431f0
BunnyeNyash <otachibosibori@gmail.com>
2023-06-27 11:33:26 -0700

    Python class MagicClass that does exactly the same as the following Python bytecode:

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,40 @@
+import math
+
+class MagicClass:
+    """
+    A class that represents a circle and provides methods to calculate its area and circumference.
+    """
+
+    def __init__(self, radius=0):
+        """
+        Initializes a MagicClass object with the given radius.
+
+        Args:
+            radius (int or float): The radius of the circle. Default is 0.
+
+        Raises:
+            TypeError: If the radius is not a number (int or float).
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """
+        Calculates the area of the circle.
+
+        Returns:
+            float: The area of the circle.
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """
+        Calculates the circumference of the circle.
+
+        Returns:
+            float: The circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
+
