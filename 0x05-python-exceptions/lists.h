0684f0aed65ef5949c6ae72d1e50549b0e0f318a
BunnyeNyash <otachibosibori@gmail.com>
2023-06-27 07:55:20 -0700

    Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

---
+++ 0x05-python-exceptions/lists.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* LISTS_H */
